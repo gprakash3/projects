7ee936ec53a4ecf073885586234959ab3c90a71e
gprakash3 <gyan.p3008@gmail.com>
2023-07-04 12:30:29 +0530

    adding DOM project to git

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+//changing header name and background color of header
+var headertitle=document.getElementById('header-title');
+headertitle.innerText='New Header';
+headertitle.style.borderBottom='solid 3px #000';
+
+
+// changing ADD ITEM to bold and green color.
+var additemclass=document.getElementsByClassName('title');
+additemclass[0].style.fontFamily="bold";
+additemclass[0].style.color='green';
+
