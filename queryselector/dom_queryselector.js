1a01624e1563cf5fa772a989816e3bec6cde8340
gprakash3 <gyan.p3008@gmail.com>
2023-07-11 13:44:31 +0530

    commit it

--- queryselector/dom_queryselector.js
+++ queryselector/dom_queryselector.js
@@ -1,14 +1,16 @@
-
+//making second list item background color to green
 var seconditem=document.querySelector('.list-group-item:nth-child(2)');
 seconditem.style.backgroundColor='green';
 
-
+//making 3rd list item invisible
 var thirditem=document.querySelector('.list-group-item:nth-child(3)');
 thirditem.style.display='none';
 
+//making 2nd item font color to green
 var items=document.querySelectorAll('li')
 items[1].style.color='green';
 
+//making alternate odd item background color to green
 var odd=document.querySelectorAll('li:nth-child(odd)');
 for(let i=0;i<odd.length;i++)
 {
